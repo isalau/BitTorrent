b48d4a78ac136fd6b41ef5c8ae67918fc7850bf2
Kiana Alikhademi <kalikhademi@ufl.edu>
2019-04-14 09:37:55 -0400

    Update DataFile.java

--- DataFile.java
+++ DataFile.java
@@ -9,10 +9,11 @@ import static java.util.Arrays.copyOf;
 
 public class DataFile {
     ArrayList<byte[]> DataInChunks = new ArrayList<>();
+    ArrayList<File> FileList = new ArrayList<>();
 
     int PieceSize;
     int FileSize;
-
+    int numOfPieces = (int) Math.ceil((double)FileSize/PieceSize);
     DataFile(int pSize, int fSize){
         PieceSize = pSize;
         FileSize = fSize;
@@ -33,7 +34,6 @@ public class DataFile {
                 }
 
             } while(readBytes != -1);
-            int numOfPieces = (int) Math.ceil((double)FileSize/PieceSize);
             if(numOfPieces > DataInChunks.size()){
                 for (int empties = DataInChunks.size(); empties < numOfPieces; empties++){
                     byte[] blanks = new byte[PieceSize];
@@ -47,5 +47,51 @@ public class DataFile {
             return false;
         }
 
+    }
+
+    public void CombineFiles(String fileName) {
+        File object = new File(fileName);
+        FileOutputStream FOutput;
+        FileInputStream FInput;
+        byte[] ReadBytes;
+        int LastReadBytes = 0;
+        for (int i = 0; i < numOfPieces; i++) {
+            File tempFile= new File(fileName + i.toString());
+            Filelist.add(tempFile);
+        }
+        try {
+            FOutput = new FileOutputStream(ofile, true);
+            for (File file : Filelist) {
+                fis = new FileInputStream(file);
+                ReadBytes = new byte[(int) file.length()];
+                LastReadBytes = FInput.read(ReadBytes, 0, (int) file.length());
+                FOutput.write(fileBytes);
+                FOutput.flush();
+                LastReadBytes = null;
+                FInput.close();
+                FInput = null;
+            }
+            fos.close();
+            fos = null;
+        } catch (Exception e) {
+            System.out.println("Could not merge the file properly");
+        }
+    }
+
+    public void WriteBytes(){
+        try{
+            // make the file name 
+            OutputStream output = new FileOutputStream(fileName);
+
+            //write the bytes into the file
+            for (byte[] bytes in DataInChunks){
+                output.write(bytes);
+                System.out.println("Wrote into the file successfully!");
+                ouput.close();
+
+            }
+        }catch(){
+             System.out.println("Exception: " + e); 
+        }
     }
 }
