a35ad7d43be429d0d7e57ca957a3f9651d4664ad
Isabel Laurenceau <isalau@ufl.edu>
2019-04-15 15:25:29 -0400

    Updated how we write piece index

--- Connection.java
+++ Connection.java
@@ -198,12 +198,8 @@ public class Connection extends Uploader implements Runnable{
 		            determineIfInterestedFromBitfield(msg);
 		            break;
 		        case 6:
-		            //got a requestMessage
 		        	System.out.println("Connection: received request message received from client: " + peerID);
 					logger.info("Peer " + sendersPeerID + " received request message from peer " + peerID);
-		        	//create piece
-		        	PieceIndex = msg[5];
-		        	System.out.println("Connection: the piece index in checkMessage"+ PieceIndex);
 		        	
 		        	sendPiece(msg);
 		            break;
@@ -573,8 +569,13 @@ public class Connection extends Uploader implements Runnable{
 			lastRequestedIndex = rand;
 			System.out.println("Connection: Asking for piece: "+ rand);
 			myBitfield[rand] = 2; 
-			requestMessage[5] = (byte) rand;
-			
+			//requestMessage[5] = (byte) rand;
+
+			byte[] indexByte = new byte[4];
+			indexByte = ByteBuffer.allocate(4).putInt(rand).array();
+			int index = java.nio.ByteBuffer.wrap(indexByte).getInt();
+
+			System.arraycopy(indexByte, 0, requestMessage,5, 4);
 			sendMessage(requestMessage);
 
 			//start timer 
@@ -600,30 +601,33 @@ public class Connection extends Uploader implements Runnable{
 	}
 
 	public void sendPiece(byte[] msg){
-		System.out.println("Connection: Sending Piece Message");
+		System.out.println("Connection: Sending Piece Message");		
 
-		int index = msg[5];
-		if(msg[5] < 0){
-			//128* 2 - the negative 
-			index = 256 + msg[5];
-		}
+		//index of piece
+		byte[] indexByte = new byte[4];
+		System.arraycopy(msg, 5, indexByte, 0, 4);
+		int index = java.nio.ByteBuffer.wrap(indexByte).getInt();
 
-		System.out.println("Connection: the PieceIndex from send piece is :"+ index);
+
+		System.out.println("Connection: The index of piece to send is: "+ index);
 
 		//create new piece message
-		int length = 4 + 1 + pieceSize; //4 for length, 1 for type, rest for piece content
+		int length = 4 + 1 + 4+ pieceSize; //4 for length, 1 for type, 4 for index,  rest for piece content
 		pieceMessage = new byte[length];
 		byte[] data = new byte[pieceSize];
+
 	 	//initalize
 		pieceMessage = ByteBuffer.allocate(length).putInt(length).array();
 		pieceMessage[4] = 7; //type seven
 
 		if(myBitfield[index] == 1){
-			System.out.println("Connection: I am in the if statement");
 			data = DataChunks.get(index);
 		}
 		
-		System.arraycopy(data, 0, pieceMessage,5, data.length);
+
+		System.arraycopy(indexByte, 0, pieceMessage,5, indexByte.length);
+
+		System.arraycopy(data, 0, pieceMessage,9, data.length);
 		sendMessage(pieceMessage);
 
 		// //start timer 
@@ -631,32 +635,36 @@ public class Connection extends Uploader implements Runnable{
 	}
 
 	public void receivedPiece(byte[] msg){
-		System.out.println("Connection: I have: "+ chunksDownloaded + " chunks downloaded");
+		System.out.println("Connection: Received Piece Message");
 		//update Peer to reflect that they get the piece 
-		byte[] data = new byte[pieceSize];
 
-		//Peer List 	
+		//get info from message		
+		byte[] indexByte = new byte[4];
+		System.arraycopy(msg, 5, indexByte, 0, 4);
+
+		byte[] data = new byte[pieceSize];
+		System.arraycopy(msg, 9, data,0, pieceSize);
+		
+		lastRequestedIndex = java.nio.ByteBuffer.wrap(indexByte).getInt();
+
+
+		System.out.println("Connection: the msg is: "+ msg);
+		System.out.println("Connection: the data is: "+ data);	
 		System.out.println("Connection: The bitfield length is: " + myBitfield.length+ " and the index is: " + lastRequestedIndex);
 		
 		//check to make sure i don't already have it 
 		if(myBitfield[lastRequestedIndex] == 0 || myBitfield[lastRequestedIndex] == 2){
 			myBitfield[lastRequestedIndex] = 1;
-			chunksDownloaded = chunksDownloaded + 1;
 		}
-
-		System.out.println("Connection: the msg is: "+ msg);
-
-		System.arraycopy(msg, 5, data,0, pieceSize);
-		System.out.println("Connection: the data is: "+ data);
-		DataChunks.set(lastRequestedIndex,data);//TODO: it is not always the last requested piece that we might receive.
-		//TODO: make sure the proper index is extracted from the body of the piece message (the first 4 bytes).
+		
+		DataChunks.set(lastRequestedIndex,data);
 		logger.info("Peer " + sendersPeerID + " has downloaded the piece " + lastRequestedIndex + " from peer " + peerID);
 
 		for(int i = 0; i < connectionLinkedList.size(); i++){
 			connectionLinkedList.get(i).sendHave(lastRequestedIndex);
 		}
 
-		String fileName2 = "Final_File.txt";//TODO: what is this???
+		String fileName2 = "Final_File.txt";
 		checkIfDone(fileName2);
 	}
 
